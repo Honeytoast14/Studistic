239e296e7a63af8c57797c48e24a79d04980bb95
Nawaporn Sriprathet <nawaporn.navis@gmail.com>
2023-12-04 18:20:06 +0700

    master question finish

---
+++ lib/englishLevel/master_question.dart
@@ -0,0 +1,109 @@
+class Question {
+  final String question;
+  final List<String> answers;
+  final int correctAnswerIndex;
+
+  Question({
+    required this.question,
+    required this.answers,
+    required this.correctAnswerIndex,
+  });
+}
+
+List<Question> questions = [
+  Question(
+    question:
+        "The most important rule to prevent your T-Shirts from _____ is to avoid heat.",
+    answers: ['shrink', 'shrank', 'shrinking', 'shrunk'],
+    correctAnswerIndex: 2,
+  ),
+  Question(
+    question:
+        "The software was designed to update the data continuously _____ the day.",
+    answers: ['every', 'throughout', 'besides', 'regarding'],
+    correctAnswerIndex: 1,
+  ),
+  Question(
+    question:
+        "Records of all _____ between the landlord and the tenants should be kept on record.",
+    answers: ['opposition', 'instruction', 'correspondence', 'substitution'],
+    correctAnswerIndex: 2,
+  ),
+  Question(
+    question:
+        "When _____ printer drivers, you have the option to delete only the printer driver or remove the entire printer-driver package.",
+    answers: ['removes', 'removable', 'removal', 'removing'],
+    correctAnswerIndex: 3,
+  ),
+  Question(
+    question:
+        "The European Union is made up of 15 nations with _____ cultural, linguistic and economic roots.",
+    answers: ['distinct', 'unclear', 'tentative', 'widespread'],
+    correctAnswerIndex: 0,
+  ),
+  Question(
+    question:
+        "Misuri Co. decided to _____ expand its product options to fulfill customers' needs.",
+    answers: ['flexibly', 'alternatively', 'strategically', 'accurately'],
+    correctAnswerIndex: 2,
+  ),
+  Question(
+    question:
+        "Hopper Tech's new database software has _____ search capabilities to provide more relevant results.",
+    answers: ['expansively', 'expanded', 'expands', 'expand'],
+    correctAnswerIndex: 1,
+  ),
+  Question(
+    question:
+        "This form is used to request _____ to students or Dartmouth visitors for travel and entertainment expense incurred for College business.",
+    answers: ['commodities', 'profits', 'offers', 'reimbursement'],
+    correctAnswerIndex: 3,
+  ),
+  Question(
+    question:
+        "Mr. Schmidt will _____ as Ms. Fabrey's replacement when she retires from her position.",
+    answers: ['make up', 'take over', 'step down', 'break through'],
+    correctAnswerIndex: 1,
+  ),
+  Question(
+    question:
+        "While some countries have made huge investments in the Caribbean lately, others have reduced _____.",
+    answers: ['themselves', 'theirs', 'their', 'they'],
+    correctAnswerIndex: 1,
+  ),
+  Question(
+    question: "The creamy richness is _____ of the cheese from this region.",
+    answers: ['vivid', 'qualified', 'substantial', 'characteristic'],
+    correctAnswerIndex: 3,
+  ),
+  Question(
+    question:
+        "As_____ a week has passed since the goods were shipped, we should ask the shipper for an update.",
+    answers: ['partially', 'immediately', 'nearly', 'thoroughly'],
+    correctAnswerIndex: 2,
+  ),
+  Question(
+    question:
+        "Several flights out of Southwest Florida International Airport are cancelled because of an _____ storm.",
+    answers: ['increasing', 'approaching', 'satisfying', 'pending'],
+    correctAnswerIndex: 1,
+  ),
+  Question(
+    question:
+        "With this 'Buy 1 Get 1 Free' promotion, customers can buy a pack of 12 black pens and get _____ free of charge.",
+    answers: ['another', 'other', 'each other', 'one another'],
+    correctAnswerIndex: 0,
+  ),
+  Question(
+    question:
+        "Frinzel Group is expected to record a _____ higher profit by the end of this year.",
+    answers: ['considerably', 'coincidentally', 'effectively', 'thoroughly'],
+    correctAnswerIndex: 0,
+  ),
+  Question(
+    question:
+        "The new design was the product of a _____ project between marketing and graphic design departments.",
+    answers: ['persuasive', 'collaborative', 'complimentary', 'reliable'],
+    correctAnswerIndex: 1,
+  ),
+];
